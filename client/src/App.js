b44932e7d920ee9c5b5f1e25fb3d733662c24425
Jonathan Silva <jonathansilvads@hotmail.com>
2023-03-06 12:37:46 -0400

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useState, useEffect } from 'react';
+
+const socket = io('http://localhost:4000');
+
+function App() {
+
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', message);
+    const newMessage ={
+      body: message,
+      from: "Me"
+    }
+    setMessages([...messages, newMessage]);
+    setMessage("");
+  };
+
+  useEffect(() => {
+
+    const receiveMsg = (message) => {
+      setMessages([...messages, message]);
+    };
+
+    socket.on('message', receiveMsg);
+
+    return () => {
+      socket.off('message', receiveMsg);
+    }
+  }, [messages]);
+
+  return (
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2x1 font-bold my-2">shoaosChat</h1>
+        <ul className='h-80 overflow-y-auto'>
+          {messages.map((message, index) => (
+            <li key={index} className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"}`}>
+              <p>{message.from}: {message.body}</p>
+            </li>
+          ))}
+        </ul>
+        <input type="text" onChange={e => setMessage(e.target.value)} value={message} className="border-2 border-zinc-500 p-2 text-black"/>
+          <button>send</button>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
